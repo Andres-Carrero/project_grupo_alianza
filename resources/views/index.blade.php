34d748fa3ea876d5ddfebfd38e8260a0199d582b
Andres Carrero <andres.carrero@gmail.com>
2025-04-26 12:15:12 -0500

    project initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('title', 'Holaa')
+
+
+<div>
+    Hola Index
+</div>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> {{ config('app.name', 'Mi Aplicación') }} - @yield('title') </title>
+</head>
+
+<body>
+    Hola Mundo
+</body>
+
+</html>
